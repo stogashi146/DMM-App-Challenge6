5d7a84723ac62ba2907be5ecad2de7b877e8f854
stogashi146 <stogashi146@gmail.com>
2021-07-06 05:43:44 +0000

    [Finish] App-Challenge6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,15 +1,16 @@
 class PostCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])# @book_comment = PostComment.new
+    @book = Book.find(params[:book_id])# @book_comment = PostComment.new
+    @book_comment = PostComment.new
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    # redirect_to book
   end
 
   def destroy
-    PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    # redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @book_comment = PostComment.find(params[:id])
+    @book_comment.destroy
   end
 
   private
